47aff7088a03ebaecfb763b9777fd0534c0270ee
Alexander Chiglintsev <alex.chiglintsev@yandex.ru>
2022-08-22 16:58:21 +0800

    Init commit

---
+++ src/test/java/ru/snx/carexpenses/CarExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.snx.carexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarExpensesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
